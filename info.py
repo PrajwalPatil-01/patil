75fe47b5470909fa5876972864f21ddc7d8c2948
PrajwalPatil-01 <prajwalpatilxp@gmail.com>
2025-01-01 07:39:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/XPmoviesrequestgroup')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/HindiHDmoviesSeriesXP')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/XPBackChannel')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'XP_Discussion_Group') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/xp_prajwal')
